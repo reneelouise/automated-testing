2c542377a6ccad49c5d145a810fa38bbfe1a7423
Renee-Louise <renee.nzems@hotmail.com>
2021-10-16 22:53:41 +0100

    passed jest tests

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -2,8 +2,8 @@
 const findShortestString = require("./find-shortest-string");
 
 test("findShortestString finds the shortest string in an array", () => {
-  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("nice");
-  expect(findShortestString(["why", "hello", "to", "you"])).toBe("hello");
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
 });
 
 test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
